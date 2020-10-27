f86a05a85be9df960f1991881fe0e19c33c8ee19
andrii.ryzhyi <andrii.nrj@gmail.com>
2020-10-27 14:13:30 +0200

    3.3. Разбор первой программы. [#354088]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Andrii Ryzhyi");
+        System.out.println("23.05.1987");
+    }
+}
